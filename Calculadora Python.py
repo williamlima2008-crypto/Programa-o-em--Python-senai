e3379a5981c4b85c4b47d9b6d7d986fad638ca52
williamlima2008-crypto <william.lima2008@gmail.com>
2026-06-14 07:04:18 -0300

    Add files via upload

---
+++ Calculadora Python.py
@@ -0,0 +1,27 @@
+print('-----------------------------------')
+print('Calculadora Python # SENAI Prof.Bea')
+print('-----------------------------------')
+num = int(input('Digite um número: '))
+num2 = int(input('Digite um número: '))
+soma = num + num2
+sub = num - num2
+div = num / num2
+divint = num // num2
+divirest = num % num2
+mult = num * num2
+raiz = num ** num2
+print(f'{num} + {num2} = {soma}')
+print(f'{num} - {num2} = {sub}')
+print(f'{num} % {num2} = {div}')
+print(f'{num}// {num2} = {divint}') # DIVISÃO SÓ COM NÚMERO INTEIRO
+print(f'{num} % {num2} = {divirest}') # RESTO DA DIVISÃO
+print(f'{num} x {num2} = {mult}')
+print(f'{num} ^ {num2} = {raiz}')
+print('-----------------------------------')
+print('Calculadora Python # SENAI Prof.Bea')
+print('-----------------------------------')
+
+
+
+
+
