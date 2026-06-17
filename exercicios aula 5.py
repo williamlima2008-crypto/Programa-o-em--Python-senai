7bd433142026fb2de1adef349083ce0449a2dc72
William <william.lima2008@gmail.com>
2026-06-17 15:18:20 -0300

    Add files via upload

---
+++ exercicios aula 5.py
@@ -0,0 +1,52 @@
+# 1
+print('Seja bem vindos!')
+
+#2
+valor = True
+print(valor)
+
+# 3
+resultado = 5.0 * 5.69
+print(resultado)
+
+# 4
+n1 = 9
+n2 = 2
+divi = n1 / n2
+print(divi)
+
+# 5
+n1 = 42
+n2 = 16
+sub = n1 - n2
+print(sub)
+
+# 6
+n1 = 42.3
+n2 = 3.5
+divint = n1 // n2
+print(divint)
+
+# 7
+n1 = 15
+n2 = 42
+n3 = 35
+n4 = 40
+mult = n1*n2*n3*n4
+print(mult)
+
+# 8
+numero = 36
+dobro = numero * 2
+print(dobro)
+
+# 9
+nome = input('Digite seu nome: ')
+email = input('Digite o email: ')
+idade = input('Digite sua idade: ')
+cidade = input('Qual sua Cidade: ')
+graduacao = input('Informe sua Graduação: ')
+estado_civil = input('Informe seu Estado civil: ')
+print(f'{nome} sua idade é {idade} anos sue email é {email}')
+print(f'Ele mora na cidade de {cidade} é {estado_civil} e se formou em{graduacao}')
+
