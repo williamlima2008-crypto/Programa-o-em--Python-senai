3dc1c4f621d84f35315c97b8ce1c0948a53b6520
William <william.lima2008@gmail.com>
2026-06-18 16:26:59 -0300

    Add files via upload

---
+++ Exercicio lista.py
@@ -0,0 +1,47 @@
+# EXERCICIO 0
+print('*'*30)
+print('Gerador de Números Pares!')
+print('*'*30)
+
+print(list(range(2, 21, 2)))
+
+
+# EXERCICIO 1
+
+print('*'*30)
+print('Lista de Números!')
+print('*'*30)
+lista = [1,2,3,4,5,6,7,8,9,10]
+print(lista)
+
+
+
+# EXERCICIO 2
+
+print('*'*30)
+print('Imprimir o terceiro elemento do exercicio 1')
+print('*'*30)
+
+print(lista[2])
+
+# EXERCICIO 3
+
+print('*'*30)
+lista.append(9)
+print(lista)
+
+
+# EXERCICIO 4
+
+print('*'*30)
+lista.remove(5)
+print(lista)
+
+
+
+# EXERCICIO 5
+
+print('*'*30)
+carros = ['Gol', 'I30', 'Corsa']
+print(f'{lista} {carros}')
+
