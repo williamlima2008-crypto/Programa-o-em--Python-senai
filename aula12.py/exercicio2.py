1948919faf2bc83f05e2973e839f3162b5bc064a
William dos Santos Lima <william.lima2008@gmail.com>
2026-06-27 14:44:52 -0300

    Add mais um exercicio.

---
+++ aula12.py/exercicio2.py
@@ -0,0 +1,17 @@
+# EXERCICIO 2
+
+def mult(n1, n2, n3):
+    return n1 * n2 * n3
+
+def calculo_mult():
+    n1 = float(input('Número: '))
+    n2 = float(input('Número: '))
+    n3 = float(input('Número: '))
+    
+    s = mult(n1, n2, n3)
+    print(s)
+    
+calculo_mult()
+    
+
+
